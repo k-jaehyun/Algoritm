5a7768972fac4cef9950e06fa6f8b156783ad4f6
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2023-11-11 19:14:34 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.06 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930#) 
 
 ### 성능 요약
 
-메모리: 77 MB, 시간: 0.04 ms
+메모리: 74.1 MB, 시간: 2.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 18:35:38
+2023년 11월 6일 19:14:32
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,24 +1,15 @@
     class Solution {
         public String solution(String s) {
         boolean evenOrNot = true;
-        char[] charArray = new char[s.length()];
+        String[] array = s.split("");
+        String answer="";
 
-
-        for (int i = 0; i < s.length(); i++) {
-
-            if (s.charAt(i) == ' ') {
-                charArray[i] = s.charAt(i);
-                evenOrNot = true;
-            } else {
-                if (evenOrNot) {
-                    charArray[i] = 'A' <= s.charAt(i) && s.charAt(i) <= 'Z' ? s.charAt(i) : (char) (s.charAt(i) - 32);
-                    evenOrNot = false;
-                } else {
-                    charArray[i] = 'a' <= s.charAt(i) && s.charAt(i) <= 'z' ? s.charAt(i) : (char) (s.charAt(i) + 32);
-                    evenOrNot = true;
-                }
-            }
+        for(String a : array) {
+            a = a.equals(" ") ? a : (evenOrNot ? a.toUpperCase() : a.toLowerCase());
+            evenOrNot= a.equals(a.toLowerCase());
+            answer+=a;
         }
-        return String.valueOf(charArray);
+        
+        return answer;
         }
     }
