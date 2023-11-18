97376266fb2da9b1dfa6602fc757feb91a90616a
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2023-11-18 22:14:03 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 15.24 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,34 +1,43 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
+                String answer = "0";
 
-        int arrLen = 1;
-
-        for(int i=1; i<food.length;i++) {
-            if(food[i]%2==1) {
-                food[i]=food[i]-1;
-                arrLen+=food[i];
-            } else {
-                arrLen+=food[i];
+        for(int i= food.length-1;i>0;i--) {
+            for (int j=0;j<food[i]/2;j++) {
+                answer = i+answer+i;
             }
         }
 
-        int[] intArr = new int[arrLen];
+        return answer;
+//         String answer = "";
 
-        int a = 0;
+//         int arrLen = 1;
 
-        for(int i=0;i<food.length-1;i++) {
-            for(int j=0;j<food[i+1]/2;j++) {
-                intArr[i+j+a]=i+1;
-                intArr[arrLen-1-i-j-a]=i+1;
-            }
-            a+=food[i+1]/2-1;
-        }
+//         for(int i=1; i<food.length;i++) {
+//             if(food[i]%2==1) {
+//                 food[i]=food[i]-1;
+//                 arrLen+=food[i];
+//             } else {
+//                 arrLen+=food[i];
+//             }
+//         }
 
-        for(int i=0;i< intArr.length;i++) {
-            answer+=String.valueOf(intArr[i]);
-        }
+//         int[] intArr = new int[arrLen];
 
-        return answer; 
+//         int a = 0;
+
+//         for(int i=0;i<food.length-1;i++) {
+//             for(int j=0;j<food[i+1]/2;j++) {
+//                 intArr[i+j+a]=i+1;
+//                 intArr[arrLen-1-i-j-a]=i+1;
+//             }
+//             a+=food[i+1]/2-1;
+//         }
+
+//         for(int i=0;i< intArr.length;i++) {
+//             answer+=String.valueOf(intArr[i]);
+//         }
+
+//         return answer; 
     }
 }
