e0a49f379982f87a8c53ff40efc2b6f03ef9cd07
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2023-11-18 22:06:55 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.34 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+
+        int arrLen = 1;
+
+        for(int i=1; i<food.length;i++) {
+            if(food[i]%2==1) {
+                food[i]=food[i]-1;
+                arrLen+=food[i];
+            } else {
+                arrLen+=food[i];
+            }
+        }
+
+        int[] intArr = new int[arrLen];
+
+        int a = 0;
+
+        for(int i=0;i<food.length-1;i++) {
+            for(int j=0;j<food[i+1]/2;j++) {
+                intArr[i+j+a]=i+1;
+                intArr[arrLen-1-i-j-a]=i+1;
+            }
+            a+=food[i+1]/2-1;
+        }
+
+        for(int i=0;i< intArr.length;i++) {
+            answer+=String.valueOf(intArr[i]);
+        }
+
+        return answer; 
+    }
+}
