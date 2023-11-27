2ef4d2a193fcda0751447d142379e708384e7c94
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2023-11-27 11:17:58 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.58 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        
+        int[] powerArray = new int[number];
+
+        for(int i=0;i<number;i++) {
+            for(int j=1;j*j<=i+1;j++) {
+                if(j*j==i+1) {
+                    powerArray[i]++;
+                } else if ((i+1) % j == 0) {
+                    powerArray[i]+=2;
+                }
+            }
+        }
+
+        for(int i=0;i<number;i++) {
+            if (powerArray[i]>limit) {
+                powerArray[i]=power;
+            }
+        }
+
+        for(int i=0;i<number;i++) {
+            answer+=powerArray[i];
+        }
+        
+        return answer;
+    }
+}
