80c94b3c2b12399340b781872cf0a77c4bfc21f5
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2023-11-29 09:37:00 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int cnt=0;
+      int cnt=0;
 
         // 0을 제외하고 당첨된 번호의 개수
         for(int i=0;i<lottos.length;i++) {
@@ -26,44 +26,11 @@ class Solution {
         yesCnt[1]=cnt;
 
         int[] answer = new int[2];
-        switch (yesCnt[0]) {
-            case 6:
-                answer[0]=1;
-                break;
-            case 5:
-                answer[0]=2;
-                break;
-            case 4:
-                answer[0]=3;
-                break;
-            case 3:
-                answer[0]=4;
-                break;
-            case 2:
-                answer[0]=5;
-                break;
-            default:
-                answer[0]=6;
-        }
-        switch (yesCnt[1]) {
-            case 6:
-                answer[1]=1;
-                break;
-            case 5:
-                answer[1]=2;
-                break;
-            case 4:
-                answer[1]=3;
-                break;
-            case 3:
-                answer[1]=4;
-                break;
-            case 2:
-                answer[1]=5;
-                break;
-            default:
-                answer[1]=6;
-        }
+        answer[0]=7-yesCnt[0];
+        answer[1]=7-yesCnt[1];
+
+        if(answer[0]>6) answer[0]=6;
+        if(answer[1]>6) answer[1]=6;
 
         return answer;
     }
