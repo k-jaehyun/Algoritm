2d255951747d48fac74fddc77ec239af99ef10e0
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2023-12-13 10:50:16 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -4,22 +4,24 @@ class Solution {
 
         String[] sArr = s.split(" ");
 
+        // char[] 변환
         char[] sCharArray= s.toCharArray();
 
+        // 첫 문자인지 판별하는 index
         int index=0;
         for (int i=0;i<s.length();i++) {
-            if (sCharArray[i]==' ') {
+            if (sCharArray[i]==' ') {  //공백이면 index를 0으로 
                 index=0;
-            } else if (index==0) {
-                if (sCharArray[i]<='z' && sCharArray[i]>='a') {
+            } else if (index==0) {  
+                if (sCharArray[i]<='z' && sCharArray[i]>='a') {  // 공백이 아니고 첫 문자일 때 대문자로
                     sCharArray[i]-=32;
                 }
-                index++;
+                index++;   // 공백이 아니기때문에 index를 올려줌
             } else {
-                if (sCharArray[i]<='Z' && sCharArray[i]>='A') {
+                if (sCharArray[i]<='Z' && sCharArray[i]>='A') {  // 첫문자가 아닐 때 대문자면 소문자로
                     sCharArray[i]+=32;
                 }
-                index++;
+                index++;   // 공백이 아니기때문에 index를 올려줌
             }
         }
 
