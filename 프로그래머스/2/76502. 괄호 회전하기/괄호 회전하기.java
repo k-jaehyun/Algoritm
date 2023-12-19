8fc4d73303a525d867a3811873eb2c5570c16a71
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2023-12-19 10:31:12 +0900

    [level 2] Title: 괄호 회전하기, Time: 12.40 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        for (int j=0;j<s.length();j++) {
+
+            Stack<Character> stack = new Stack<>();
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+
+                if (stack.isEmpty()) {
+                    stack.push(c);
+                } else if (stack.peek() == '(' && c == ')') {
+                    stack.pop();
+                } else if (stack.peek() == '{' && c == '}') {
+                    stack.pop();
+                } else if (stack.peek() == '[' && c == ']') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+            if (stack.isEmpty()) answer++;
+
+            s=s.substring(1)+s.charAt(0);
+        }
+
+        return answer;
+    }
+}
