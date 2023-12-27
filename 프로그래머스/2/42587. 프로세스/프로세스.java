b5ba267c622756e29630f0c478eeeb0ce2a1af63
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2023-12-28 00:04:34 +0900

    [level 2] Title: 프로세스, Time: 0.11 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -4,30 +4,29 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        // map에 (key:위치, value:값)으로 넣어놓음
+        // map에 (key:위치, value:우선순위)으로 넣어놓음 (while 조건 비교를 위해)
         Map<Integer,Integer> map = new HashMap<>();
         for (int i=0;i< priorities.length;i++) {
             map.put(i,priorities[i]);
         }
 
-        int orderIndex = 0;
+        int orderIndex = 0; // 몇번째 값을 검증할건지
         while (map.get(location)!=0) {
-            int doIndex=0;
+            int doIndex=0; // 실행할지말지
             for (int i=0;i< priorities.length;i++) {
                 doIndex++;
-                if (priorities[orderIndex]<priorities[i]) {
+                if (priorities[orderIndex]<priorities[i]) {  // 더 높은 우선순위가 있다면 doindex를 0으로
                     doIndex=0;
                     break;
                 }
             }
-            if (doIndex == priorities.length) {
+            if (doIndex == priorities.length) {  // 더 높은 우선순위가 없었다면 실행
                 answer++;
-                map.put(orderIndex,0);
-                priorities[orderIndex]=0;
-
+                map.put(orderIndex,0);  // while에서 비교를 위해 0으로 만듦
+                priorities[orderIndex]=0;  // 우선순위를 0으로 만들어버림
             }
             orderIndex++;
-            if (orderIndex == priorities.length) {
+            if (orderIndex == priorities.length) {  // 한바퀴 다 돌았으면 다시 처음으로
                 orderIndex=0;
             }
         }
