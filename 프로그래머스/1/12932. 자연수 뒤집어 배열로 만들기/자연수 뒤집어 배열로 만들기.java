6ae593955fac8a93db3134a452a88c91ceba6138
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-04-17 00:17:37 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 2.71 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -3,23 +3,7 @@ import java.util.*;
 class Solution {
     public int[] solution(long n) {
         
-        // n의 자리수 구하기
-        int index=0;
-        for(int i=1;;i++){
-            if(n/(Math.pow(10,i-1)) < 1) {
-                index=i-1;
-                break;
-            }
-        }
-        
-        int[] answer = new int[index];
-        
-        String[] array = String.valueOf(n).split("");
-        for(int i=0;i<index;i++) {
-            answer[i] = Integer.parseInt(array[index-i-1]);
-        }
-        
-        
-        return answer;
+return new StringBuilder().append(n).reverse().chars()
+				.map(a -> Integer.parseInt(String.valueOf((char)a))).toArray();
     }
 }
