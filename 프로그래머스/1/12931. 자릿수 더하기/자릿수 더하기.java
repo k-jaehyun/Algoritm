043fa405c7bbb2827db3c77b26b44e2fca81d2cc
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-04-16 16:03:49 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.15 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,19 +4,9 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        
-        int m = (int) Math.log10(n);
-        System.out.println(m);
-        
-        for(int i=m+1;i>0;i--) {
-            int q = n / (int) Math.pow(10,i-1);
-            answer+=q;
-        System.out.println(q);
-            
-            n=n- (int)(Math.pow(10,i-1)*q);
-        // System.out.println(n);
-        
+        String[] array = String.valueOf(n).split("");
+        for(String s : array) {
+            answer+=Integer.parseInt(s);
         }
 
         return answer;
