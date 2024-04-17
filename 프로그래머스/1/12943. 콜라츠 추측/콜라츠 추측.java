5533190f57393090a5fc9bae94b3aa71f56061cb
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-04-17 12:19:54 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n=(long)num;
+        
+        while(n != 1) {
+            if(n%2 == 0) {
+                n = n/2;
+                answer++;
+            } else if (n%2 !=0) {
+                n = n*3+1;
+                answer++;
+            }
+            
+            if(answer>500) {
+                return -1;
+            }
+        }
+        
+        return answer;
+    }
+}
