86269d2c2bd1e82be10a8a9376dde75fb9973253
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-04-23 16:03:02 +0900

    [level 1] Title: 시저 암호, Time: 29.52 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -2,21 +2,28 @@ class Solution {
     public String solution(String s, int n) {
         String answer = "";
         
-                    for (int i=0;i<s.length();i++) {
-                if(s.charAt(i)==' ') {
-                    answer+=" ";
-                } else if (s.charAt(i)<='Z') {
-                    if (s.charAt(i)+n<=90) {
-                        answer+=Character.toString((s.charAt(i)+n));
-                    }
-                    else answer+=Character.toString((s.charAt(i)+n-26));
-                } else {
-                    if (s.charAt(i)+n<=122) {
-                        answer+=Character.toString((s.charAt(i)+n));
-                    }
-                    else answer+=Character.toString((s.charAt(i)+n-26));
+        for(int i=0;i<s.length();i++) {
+            char c = s.charAt(i);
+            if(c==' ') {
+                answer+=c;
+            }else if(c<='Z') {
+                c+=n;
+                if('Z'<c) {
+                    c-=26;
+                    answer+=c;
+                }else {
+                    answer+=c;
+                }
+            }else {
+                c+=n;
+                if('z'<c) {
+                    c-=26;
+                    answer+=c;
+                }else {
+                    answer+=c;
                 }
             }
+        }
         
         return answer;
     }
