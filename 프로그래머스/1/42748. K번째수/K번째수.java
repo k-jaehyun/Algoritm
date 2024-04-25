29c4f76df2c5a6fa8928d549704b89ae51cdf30d
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-04-25 15:08:41 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,41 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
 
-        List<Integer> integerList = new ArrayList<>();
-
-        for(int[] com : commands) {
-            int a =com[0];
-            int b = com[1];
-            int c = com[2];
-
-            int[] temp = new int[b-a+1];
-
-            for(int i=a-1;i<b;i++) {
-                temp[i-a+1]= array[i];
+        for(int i=0;i<commands.length;i++) {
+            List<Integer> comArr = new ArrayList<Integer>();
+            for(int j=0;j<commands[i][1]-commands[i][0]+1;j++) {
+                comArr.add(array[commands[i][0]+j-1]);
             }
-
-            for(int i=0;i<temp.length;i++) {
-                for (int j=i+1;j< temp.length;j++) {
-                    if (temp[i]>temp[j]) {
-                        int t = temp[j];
-                        temp[j] = temp[i];
-                        temp[i] = t;
-                    }
-                }
-            }
-
-            integerList.add(temp[c-1]);
+            Collections.sort(comArr);
+            answer[i] = comArr.get(commands[i][2]-1);
         }
-
-        for(int i=0;i<integerList.size();i++) {
-            answer[i]=integerList.get(i);
-        }
-
-        
         
         return answer;
     }
