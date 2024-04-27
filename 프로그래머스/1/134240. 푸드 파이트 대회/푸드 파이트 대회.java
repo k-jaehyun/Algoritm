f6cf833142115ec271208d0edc45eedcc2246bfb
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-04-27 16:24:20 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 13.40 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,43 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-                String answer = "0";
-
-        for(int i= food.length-1;i>0;i--) {
-            for (int j=0;j<food[i]/2;j++) {
-                answer = i+answer+i;
+        String answer = "0";
+        
+        for(int i=0;i<food.length-1;i++) {
+            for(int j=0;j<food[food.length-1-i]/2;j++) {
+                answer= (food.length-1-i) + answer + (food.length-1-i);
             }
         }
-
+        
         return answer;
-//         String answer = "";
-
-//         int arrLen = 1;
-
-//         for(int i=1; i<food.length;i++) {
-//             if(food[i]%2==1) {
-//                 food[i]=food[i]-1;
-//                 arrLen+=food[i];
-//             } else {
-//                 arrLen+=food[i];
-//             }
-//         }
-
-//         int[] intArr = new int[arrLen];
-
-//         int a = 0;
-
-//         for(int i=0;i<food.length-1;i++) {
-//             for(int j=0;j<food[i+1]/2;j++) {
-//                 intArr[i+j+a]=i+1;
-//                 intArr[arrLen-1-i-j-a]=i+1;
-//             }
-//             a+=food[i+1]/2-1;
-//         }
-
-//         for(int i=0;i< intArr.length;i++) {
-//             answer+=String.valueOf(intArr[i]);
-//         }
-
-//         return answer; 
     }
 }
