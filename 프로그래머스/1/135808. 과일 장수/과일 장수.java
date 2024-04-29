9879625d9b39fb64748d544054681c7909207e72
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-04-29 16:08:24 +0900

    [level 1] Title: 과일 장수, Time: 0.37 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -4,35 +4,11 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         
-         List<Integer> arr = new ArrayList<>();
-
-        for (int i : score) {
-            arr.add(i);
-        }
-
-//        while(arr.size()>=m) {
-//            int boxLast=0;
-//            int count=0;
-//            for (int i=0;i<m;i++) {
-//                if (!arr.isEmpty()) {
-//                    boxLast = arr.get(0);
-//                    arr.remove(0);
-//                    count++;
-//                }
-//            }
-//            answer+=boxLast*count;
-//        }
-
-        Collections.sort(arr,Collections.reverseOrder());
-
-        int boxes = arr.size() / m;
-
-        for (int i = 1; i < boxes+1; i++) {
-            int boxLast = arr.get((i * m)-1);
-            answer += boxLast * m;
-        }
+        Arrays.sort(score);
         
-        
+        for(int i=1;i<=score.length/m;i++) {
+            answer += score[score.length-(m*i)] *m;
+        }
         
         return answer;
     }
