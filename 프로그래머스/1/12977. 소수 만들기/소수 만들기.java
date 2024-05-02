19b4332676683e7d98a03855d452db7c43a3ae48
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-02 15:10:15 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,25 +1,24 @@
 class Solution {
     public int solution(int[] nums) {
-        int answer = -1;
-
-        int tmp=0;
-        int count=0;
-
-        for(int i=0;i< nums.length;i++) {
-            for(int j=i+1;j< nums.length;j++) {
-                Loop1: for (int k=j+1;k< nums.length;k++) {
-                    tmp = nums[i]+nums[j]+nums[k];
-                    for(int l=2;l<tmp;l++) {
-                        if(tmp%l==0) {
-                            continue Loop1;
+        int answer = 0;
+        
+        for(int i=0;i<nums.length;i++) {
+            for(int j=i+1;j<nums.length;j++) {
+                for(int k=j+1;k<nums.length;k++) {
+                    int x = nums[i]+nums[j]+nums[k];
+                    int tmp=0;
+                    for(int l=2;l*l<=x;l++) {
+                        if(x%l==0) {
+                            tmp++;
+                            break;
                         }
                     }
-                    count++;
+                    if(tmp==0) {
+                        answer++;
+                    }
                 }
             }
         }
-        
-        answer=count;
 
         return answer;
     }
