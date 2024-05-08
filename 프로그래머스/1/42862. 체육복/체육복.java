fcf51dd9fe9e531916f2b2e5c64dc03688de7e3b
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-08 23:18:00 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,43 +1,37 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-
+        
         int[] students = new int[n];
-
-        // 강탈
-        for (int i=0;i<lost.length;i++) {
+        
+        for(int i=0;i<n;i++) {
+            students[i]=1;
+        }
+        
+        for(int i=0;i<lost.length;i++) {
             students[lost[i]-1]--;
         }
-
-        // 더 있는애들
-        for (int i=0;i< reserve.length;i++) {
+        
+        for(int i=0;i<reserve.length;i++) {
             students[reserve[i]-1]++;
         }
         
-        //빌려옴
-        if (students[0]<0 && students[1]>0) {
-            students[0]++;
-            students[1]--;
-        }
-        for (int i=1;i<n-1;i++) {
-            if(students[i]<0) {
-                if(students[i-1]>0) {
-                    students[i]++;
+        for(int i=0;i<n;i++) {
+            if(students[i]==0) {
+                if(i-1>=0 && students[i-1]==2) {
                     students[i-1]--;
-                } else if (students[i+1]>0) {
                     students[i]++;
+                } else if(i+1<n && students[i+1]==2) {
                     students[i+1]--;
+                    students[i]++;
                 }
             }
         }
-        if (students[n-1]<0 && students[n-2]>0) {
-            students[n-1]++;
-            students[n-2]--;
-        }
         
-        // 0 이상인 애들 카운트
-        for (int s : students) {
-            if (s>=0) answer++;
+        for(int num : students) {
+            if(num>=1) {
+                answer++;
+            }
         }
         
         return answer;
