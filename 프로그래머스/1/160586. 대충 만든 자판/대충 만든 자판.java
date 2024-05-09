e056e057b23ca0c3d278e6843577ff2d4235796f
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-09 16:09:25 +0900

    [level 1] Title: 대충 만든 자판, Time: 19.68 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,34 +1,33 @@
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
+
         int[] answer = new int[targets.length];
-
-        //target 배열 하나씩
-        loop1: for (int q=0;q< targets.length;q++) {
-
-            int sumEachTouch=0;
-
-            //"ABCD"에서 하나씩
-            for (int i=0;i<targets[q].length();i++) {
-                int minTouch = 1000;
-
-                //keymap 배열 하나씩
-                for (int j = 0; j < keymap.length; j++) {
-
-                    //"ABACD"에서 하나씩
-                    for (int k = 0; k < keymap[j].length(); k++) {
-                        if (keymap[j].charAt(k) == targets[q].charAt(i) && (k + 1) < minTouch) {
-                            minTouch = k + 1;
-                            continue;
+        int min;
+        int index;
+        
+        loop1: for(int i=0;i<targets.length;i++) {
+            loop2: for(int j=0;j<targets[i].length();j++) {
+                min=Integer.MAX_VALUE;
+                loop3: for(int k=0;k<keymap.length;k++) {
+                    index=0;
+                    for(int l=0;l<keymap[k].length();l++) {
+                        index++;
+                        if(keymap[k].charAt(l)==targets[i].charAt(j)) {
+                            if(index<min) {
+                                min=index;
+                            }
+                            index=0;
+                            continue loop3;
                         }
+                        
                     }
                 }
-                if (minTouch == 1000) {
-                    answer[q] = -1;
+                answer[i]+=min;
+                if(min==Integer.MAX_VALUE) {
+                    answer[i]=-1;
                     continue loop1;
                 }
-                sumEachTouch+=minTouch;
             }
-                answer[q] = sumEachTouch;
         }
         
         return answer;
