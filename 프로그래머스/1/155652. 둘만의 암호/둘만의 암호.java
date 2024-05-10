234be36419a5f369ff97913d4f7c7e35c60b7528
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-10 23:41:54 +0900

    [level 1] Title: 둘만의 암호, Time: 0.09 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,19 +1,25 @@
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
-
-    char[] chars = s.toCharArray();
-
-        for (char c : chars) {
-                for (int i = 0; i < index; i++) {
-        c += 1;
-        if (c>'z') c-=26;
-        if (skip.contains(String.valueOf(c))) i--;
-        }
-        answer += c;
-        }
-
+        StringBuilder answer = new StringBuilder();
         
-        return answer;
+        for(int i=0; i<s.length(); i++) {
+            char currentChar = s.charAt(i);
+            int add = index;
+            
+            while(add > 0) {
+                currentChar = (char) (currentChar + 1);
+                if(currentChar > 'z') {
+                    currentChar = 'a';
+                }
+                
+                if(skip.indexOf(currentChar) == -1) {
+                    add--;
+                }
+            }
+            
+            answer.append(currentChar);
+        }
+        
+        return answer.toString();
     }
 }
