949d8f4516808b55ff043daea4700f473192224c
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-13 23:05:13 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 2.44 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,10 +1,11 @@
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
 
 class Solution {
-    public String solution(String[] survey, int[] choices) {
-        Map<String,Integer> map = new HashMap<>();
+    public String solution(String[] survey, int[] choices) {        
+        
+        String answer = "";
+        
+        Map<String, Integer> map = new HashMap<>();
         map.put("R",0);
         map.put("T",0);
         map.put("C",0);
@@ -13,61 +14,47 @@ class Solution {
         map.put("M",0);
         map.put("A",0);
         map.put("N",0);
-
-        for (int i=0;i<survey.length;i++) {
-            int leftCurrentValue=map.get(Character.toString(survey[i].charAt(0)));
-            int rightCurrentValue=map.get(Character.toString(survey[i].charAt(1)));
-                switch (choices[i]) {
-                    case 1:
-                        map.put(Character.toString(survey[i].charAt(0)),leftCurrentValue+3);
-                        break;
-                    case 2:
-                        map.put(Character.toString(survey[i].charAt(0)),leftCurrentValue+2);
-                        break;
-                    case 3:
-                        map.put(Character.toString(survey[i].charAt(0)),leftCurrentValue+1);
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        map.put(Character.toString(survey[i].charAt(1)),rightCurrentValue+1);
-                        break;
-                    case 6:
-                        map.put(Character.toString(survey[i].charAt(1)),rightCurrentValue+2);
-                        break;
-                    case 7:
-                        map.put(Character.toString(survey[i].charAt(1)),rightCurrentValue+3);
-                        break;
-                }
+        
+        for(int i=0;i<survey.length;i++) {
+            if(choices[i]==7) {
+                map.put(survey[i].substring(1,2), map.getOrDefault(survey[i].substring(1,2),0)+3);
+            } else if(choices[i]==6) {
+                map.put(survey[i].substring(1,2), map.getOrDefault(survey[i].substring(1,2),0)+2);
+            } else if(choices[i]==5) {
+                map.put(survey[i].substring(1,2), map.getOrDefault(survey[i].substring(1,2),0)+1);
+            } else if(choices[i]==3) {
+                map.put(survey[i].substring(0,1), map.getOrDefault(survey[i].substring(0,1),0)+1);
+            } else if(choices[i]==2) {
+                map.put(survey[i].substring(0,1), map.getOrDefault(survey[i].substring(0,1),0)+2);
+            } else if(choices[i]==1) {
+                map.put(survey[i].substring(0,1), map.getOrDefault(survey[i].substring(0,1),0)+3);
+            }
         }
-
-        StringBuilder answer = new StringBuilder();
-
-        if (map.get("R")>=map.get("T")) {
-            answer.append("R");
+        
+        if(map.get("R")>=map.get("T")) {
+            answer+="R";
         } else {
-            answer.append("T");
+            answer+="T";
         }
 
-        if (map.get("C")>=map.get("F")) {
-            answer.append("C");
+        if(map.get("C")>=map.get("F")) {
+            answer+="C";
         } else {
-            answer.append("F");
+            answer+="F";
         }
 
-        if (map.get("J")>=map.get("M")) {
-            answer.append(("J"));
+        if(map.get("J")>=map.get("M")) {
+            answer+="J";
         } else {
-            answer.append(("M"));
+            answer+="M";
         }
 
-        if (map.get("A")>=map.get("N")) {
-            answer.append(("A"));
+        if(map.get("A")>=map.get("N")) {
+            answer+="A";
         } else {
-            answer.append(("N"));
+            answer+="N";
         }
 
-
-        return answer.toString();
+        return answer;
     }
 }
