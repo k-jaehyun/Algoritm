8da3bcb2851f9141be3a57a27a6b5de7c3faa1e1
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-17 19:12:47 +0900

    [level 1] Title: 공원 산책, Time: 0.08 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,51 +1,61 @@
 class Solution {
     public int[] solution(String[] park, String[] routes) {
-            int[] answer = {0,0};
-
-            // 시작 지점을 숫자로
-            int startX = 0;
-            int startY = 0;
-
-            findStartPoint:
-            for (int i = 0; i < park.length; i++) {
-                for (int j = 0; j < park[i].length(); j++) {
-                    if (park[i].charAt(j) == 'S') {
-                        startY = i;
-                        startX = j;
-                        break findStartPoint;
-                    }
+        
+        int[] start = {0,0};
+        for(int i=0;i<park.length;i++) {
+            for(int j=0;j<park[i].length();j++) {
+                if(park[i].charAt(j)=='S') {
+                    start[0]=i;
+                    start[1]=j;
                 }
             }
-
-            // 이동 시키기
-            move:
-            for (String route : routes) {
-                String direction = route.substring(0, 1);
-                int moving = Integer.valueOf(route.substring(2));
-
-                int tempX=startX;
-                int tempY=startY;
-
-                for (int i =0;i<moving;i++) {
-                    if (direction.equals("E")) tempX++;
-                    if (direction.equals("W")) tempX--;
-                    if (direction.equals("N")) tempY--;
-                    if (direction.equals("S")) tempY++;
-
-                    if (tempX>=0 && tempY>=0 && tempX<park[0].length() && tempY<park.length) {
-                        if (park[tempY].charAt(tempX) == 'X') continue move;
+        }
+        
+        loop: for(String r : routes) {
+            if(r.contains("E")) {
+                int tmp=start[1];
+                for(int i=0;i<r.charAt(2)-'0';i++) {
+                    if(tmp+1>=park[0].length() || park[start[0]].charAt(tmp+1)=='X') {
+                       continue loop;
+                    } else {
+                        tmp+=1;
                     }
                 }
-
-                if (tempX>=0 && tempY>=0 && tempX<park[0].length() && tempY<park.length) {
-                    startX=tempX;
-                    startY=tempY;
+                start[1]=tmp;
+            } else if(r.contains("W")) {
+                int tmp=start[1];
+                for(int i=0;i<r.charAt(2)-'0';i++) {
+                    if(tmp-1<0 || park[start[0]].charAt(tmp-1)=='X') {
+                       continue loop;
+                    } else {
+                        tmp-=1;
+                    }
                 }
+                start[1]=tmp;
+            } else if(r.contains("S")) {
+                int tmp = start[0];
+                for(int i=0;i<r.charAt(2)-'0';i++) {
+                    if(tmp+1>=park.length || park[tmp+1].charAt(start[1])=='X') {
+                        continue loop;
+                    } else {
+                        tmp+=1;
+                    }
+                }
+                start[0]=tmp;
+            } else if(r.contains("N")) {
+                int tmp = start[0];
+                for(int i=0;i<r.charAt(2)-'0';i++) {
+                    if(tmp-1<0 || park[tmp-1].charAt(start[1])=='X') {
+                        continue loop;
+                    } else {
+                        tmp-=1;
+                    }
+                }
+                start[0]=tmp;
             }
-
-            answer[0]=startY;
-            answer[1]=startX;
-
-            return answer;
+        }
+        
+        
+        return start;
     }
 }
