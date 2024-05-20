fa4b5031d61d9a3859146f842d8fecd60ac0b5e2
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-20 23:12:57 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.70 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,32 +1,24 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
+        
         String answer = "";
-
-        String[] sArr = s.split(" ");
-
-        // char[] 변환
-        char[] sCharArray= s.toCharArray();
-
-        // 첫 문자인지 판별하는 index
-        int index=0;
-        for (int i=0;i<s.length();i++) {
-            if (sCharArray[i]==' ') {  //공백이면 index를 0으로 
+        
+        String[] strArr = s.toLowerCase().split("");
+        int index = 0;
+        for(String str : strArr) {
+           if(index==0) {
+               str=str.toUpperCase();
+               index=1;
+            }
+            answer+=str;
+            
+            if(str.equals(" ")) {
                 index=0;
-            } else if (index==0) {  
-                if (sCharArray[i]<='z' && sCharArray[i]>='a') {  // 공백이 아니고 첫 문자일 때 대문자로
-                    sCharArray[i]-=32;
-                }
-                index++;   // 공백이 아니기때문에 index를 올려줌
-            } else {
-                if (sCharArray[i]<='Z' && sCharArray[i]>='A') {  // 첫문자가 아닐 때 대문자면 소문자로
-                    sCharArray[i]+=32;
-                }
-                index++;   // 공백이 아니기때문에 index를 올려줌
             }
         }
-
-        answer=String.valueOf(sCharArray);
-
+        
         return answer;
     }
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 76.9 MB, 시간: 0.04 ms
+메모리: 71.2 MB, 시간: 1.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 10:50:12
+2024년 05월 20일 23:12:52
 
 ### 문제 설명
 
