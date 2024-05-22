86ddf35875cf9b41cda3b78ea705d04e90cf9272
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-22 15:26:09 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,34 +1,25 @@
-import java.util.HashMap;
-import java.util.Map;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = {0,0};
+        int total = brown+yellow;
         
-        Map<Integer, Integer> wholeMap = new HashMap<>();
-        for (int i = 3; i <= brown + yellow; i++) {      
-            int j = (brown + yellow) / i;
-            if (i>=j && i * j == brown + yellow) {
-                wholeMap.put(i, j);
-            }
-        }
-
-        Map<Integer, Integer> yellowMap = new HashMap<>();
-        for (int i = 1; i <= yellow; i++) {
-            int j = (yellow) / i;
-            if (i>=j && i * j == yellow) {
-                yellowMap.put(i, j);
-            }
-        }
-
-        for (Map.Entry<Integer, Integer> wholeEntry : wholeMap.entrySet()) {
-            for (Map.Entry<Integer, Integer> yellowEntry : yellowMap.entrySet()) {
-                if (wholeEntry.getKey() == yellowEntry.getKey() + 2 && wholeEntry.getValue() == yellowEntry.getValue() + 2) {
-                    answer[0] = wholeEntry.getKey();
-                    answer[1] = wholeEntry.getValue();
-                    return answer;
+                // yellow를 만들 수 있는 가로세로
+                for(int i=1;i*i<=yellow;i++) {
+                    if(i*(yellow/i)==yellow) {
+                        
+                        if((i+2)*((yellow/i)+2)==total) {
+                            answer[0]=(yellow/i)+2;
+                            answer[1]=i+2;
+                        }
+                        
+                    }
                 }
-            }
-        }
+
+        
+
+        
+        
+        
         
         return answer;
     }
