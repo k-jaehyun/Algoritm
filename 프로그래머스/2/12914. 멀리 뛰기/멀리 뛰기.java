e0329de3ee8949cbae9a4b24f0aab6238cba9187
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-23 18:02:38 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,23 +1,19 @@
 class Solution {
     public long solution(int n) {
         long answer = 0;
-        if (n==1) return 1;
-        if (n==2) return 2;
-
-        // 1 - 1   2 - 2   3 - 3  4 - 5  5 - 8
-
-        // n = n-1 + n-2
-        long[] longs = new long[n+1];
-
-        longs[1]=1;
-        longs[2]=2;
-
-        for (int i=3;i< longs.length;i++) {
-            longs[i]=(longs[i-1]+longs[i-2])%1234567;
+        
+        if(n==1) return 1;
+        if(n==2) return 2;
+        
+        // 1-1  2-2  3-3  4-5  5-8
+        long[] num = new long[n];
+        num[0]=1;
+        num[1]=2;
+        for(int i=2;i<n;i++) {
+            num[i]=(num[i-1]+num[i-2])%1234567;
         }
-
-        answer=longs[n];
-
+        answer = num[n-1];
+        
         return answer;
     }
 }
