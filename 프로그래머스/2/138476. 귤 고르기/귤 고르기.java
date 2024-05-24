9847e9e0a83263c423ebf0e0404d577031008b05
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-24 19:36:35 +0900

    [level 2] Title: 귤 고르기, Time: 55.41 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -4,23 +4,26 @@ class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         
-        // map에 담는다
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i=0;i< tangerine.length;i++) {
-            int a = tangerine[i];
-            map.put(a,map.getOrDefault(a,0)+1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int size : tangerine) {
+            map.put(size, map.getOrDefault(size,0)+1);
         }
-
-        // 많은 것 부터 채워야한다
-        // List로 변형
-        List<Integer> valueList = new ArrayList<>(map.values());
-        Collections.sort(valueList,Collections.reverseOrder());
-
-        for (int i=0;i< valueList.size();i++) {
-            k=k-valueList.get(i);
+        
+        List<Integer> list = new ArrayList<>();
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int num = entry.getValue();
+            list.add(num);
+        }
+        
+        Collections.sort(list, Collections.reverseOrder());
+        for(int i=0;i<list.size();i++) {
+            k-=list.get(i);
             answer++;
-            if (k<1) break;
+            if(k<=0) {
+                break;
+            }
         }
+        
         
         return answer;
     }
