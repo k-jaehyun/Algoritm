cb1d9f6e23086ea8055e27183e9112c002efc8ae
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-27 11:55:58 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,38 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        int sumNum = 0;
-        for (int num : number) {
-            sumNum+=num;
-        }
-
-        // 회원등록 할지말지 고민 할 수 있는 날짜
-        int compDays = discount.length - sumNum + 1;
-
-        // 원하는 목록이 횟수만큼 모두 담긴 wantAll을 만듦
-        String[] wantAll = new String[sumNum];
-        int index=0;
-        for (int i=0;i<want.length;i++) {
-            for (int j=0;j<number[i];j++) {
-                wantAll[index]=want[i];
-                index++;
-            }
-        }
-        
-        for (int k=0;k<compDays;k++) {  // 회원가입 할지 비교
-            int cnt = 0;
-            String[] copy = wantAll.clone();  // 같으면 ""로 바꿔주면서 비교하려고 깊은복사 
-            for (int i = k; i < k+sumNum; i++) {   // discount와 비교, 시작 위치
-                for (int j = 0; j < copy.length; j++) {  // 원하는 물품인지 비교
-                    if (discount[i].equals(copy[j])) {
-                        copy[j] = "";
-                        cnt++;
-                        break;
+        loop2: for(int i=0;i<=discount.length-10;i++) {
+            int[] tmpNum = Arrays.copyOf(number, number.length);
+            for(int j=0;j<10;j++) {
+                    loop1: for(int k=0;k<want.length;k++) {
+                        if(discount[i+j].equals(want[k])) {
+                            tmpNum[k]--;
+                            break loop1;
+                        }
                     }
+            }
+            for(int j=0;j<tmpNum.length;j++) {
+                if(tmpNum[j]>0) {
+                    continue loop2;
                 }
             }
-            if (cnt==sumNum) answer++;  
+            answer++;
         }
         
         return answer;
