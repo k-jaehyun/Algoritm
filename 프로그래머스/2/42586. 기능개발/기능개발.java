b7b917cf2da51c258fdd93743b1603a2530402b0
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-28 16:52:41 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,38 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] temp = new int[100];  // 모두 0으로 초기화 되어 있음.
-
-        // 작업 진행 상황을 모두 배열로(0 포함)
-        for (int i=0;i<100;i++) {
-            int index=0;
-            int endIndex=0;
-
-            // 진도를 모두 올림
-            for (int j=0;j< progresses.length;j++) {
-                progresses[j]+=speeds[j];
+        
+        List<Integer> list = new ArrayList<>();
+        int end = 0;
+        while(end<progresses.length) {
+            int tmp=0;
+            for(int i=0;i<progresses.length;i++) {
+                progresses[i] += speeds[i];
             }
-
-            // 올라간 진도를 비교해서...
-            for (int j=0;j< progresses.length;j++) {
-                if (progresses[j] >= 100 && progresses[j] <= 20000) {  //진도율 100이던 애가 속도 100으로 100일 달리면 20000 (최대)
-                    index++;
-                    endIndex++;
-                    progresses[j] = 30000; // if문을 벗어나게 아예 크게 설정
-                } else if (progresses[j]<100) {
-                    temp[i]=index;
-                    break;
-                } else {
-                    endIndex++;
+            for(int i=0;i<progresses.length;i++) {
+                if(progresses[i]>=100) {
+                    if(progresses[i]>=10000) {
+                        continue;
+                    }
+                    end++;
+                    tmp++;
+                    progresses[i]=10000;
+                    continue;
                 }
-                
-                if (endIndex== progresses.length) {
-                    temp[i]=index;
-                }
+                break;
+            }
+            
+            if(tmp!=0) {
+                list.add(tmp);
             }
         }
-
-        // temp에서 0 제거
-        return Arrays.stream(temp).filter(a-> a!=0).toArray();
+        
+        int[] answer = new int[list.size()];
+        for(int i=0;i<list.size();i++) {
+            answer[i]=list.get(i);
+        }
+        
+        return answer;
     }
 }
