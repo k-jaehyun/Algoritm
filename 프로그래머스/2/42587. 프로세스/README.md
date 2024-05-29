1b3c021ba91cf79216df1756c452663ec94adf98
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-05-29 14:53:52 +0900

    [level 2] Title: 프로세스, Time: 7.87 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587#) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 0.11 ms
+메모리: 72 MB, 시간: 7.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 0:4:31
+2024년 05월 29일 14:53:47
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,33 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
+        int len = priorities.length;
         
-        // map에 (key:위치, value:우선순위)으로 넣어놓음 (while 조건 비교를 위해)
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i=0;i< priorities.length;i++) {
-            map.put(i,priorities[i]);
-        }
-
-        int orderIndex = 0; // 몇번째 값을 검증할건지
-        while (map.get(location)!=0) {
-            int doIndex=0; // 실행할지말지
-            for (int i=0;i< priorities.length;i++) {
-                doIndex++;
-                if (priorities[orderIndex]<priorities[i]) {  // 더 높은 우선순위가 있다면 doindex를 0으로
-                    doIndex=0;
-                    break;
+        for(int j=0;;j++) {
+            System.out.println("s : "+j);
+            int max=priorities[j%len];
+            for(int i=j;i<len+j;i++) {
+                if(priorities[i%len]>max) {
+                    max=priorities[i%len];
+                    j=i;
                 }
             }
-            if (doIndex == priorities.length) {  // 더 높은 우선순위가 없었다면 실행
-                answer++;
-                map.put(orderIndex,0);  // while에서 비교를 위해 0으로 만듦
-                priorities[orderIndex]=0;  // 우선순위를 0으로 만들어버림
-            }
-            orderIndex++;
-            if (orderIndex == priorities.length) {  // 한바퀴 다 돌았으면 다시 처음으로
-                orderIndex=0;
+            
+            priorities[j%len]=0;
+            answer++;
+            System.out.println(j);
+            
+            if(j%len==location)  {
+                break;
             }
         }
         
