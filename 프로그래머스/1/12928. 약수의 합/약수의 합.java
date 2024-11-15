4e2814c29bdc3b19b8c9fedd33e3ab0492fb0206
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-11-15 10:13:33 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -2,9 +2,11 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i=1;i<=n;i++) {
-            if(n%i==0) {
+        for(int i=1;i*i<=n;i++) {
+            if(i*i==n) {
                 answer+=i;
+            } else if(n%i == 0) {
+                answer+= (n/i + i);
             }
         }
         
