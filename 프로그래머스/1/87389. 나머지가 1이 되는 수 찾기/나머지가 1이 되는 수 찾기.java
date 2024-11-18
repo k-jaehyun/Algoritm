9f44b95840e0255a0006cd803baa5214dba72cf2
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-11-18 09:58:22 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 6.66 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -1,11 +1,17 @@
 class Solution {
     public int solution(int n) {
         
-        for(int i=2;;i++) {
-            if(n%i==1) {
-                return i;
+        int answer = 0;
+        
+        for(int i=2;i<n;i++) {
+            if(n%i ==1 ) {
+                answer = i;
+                break;
             }
         }
+
+        
+        return answer;
         
     }
 }
