d1f933b621e090d961a3c91eed5b718bdf3f2da9
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-11-22 10:59:09 +0900

    [level 1] Title: 하샤드 수, Time: 5.87 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,20 +1,5 @@
-import java.util.*;
-
 class Solution {
     public boolean solution(int x) {
-        boolean answer = false;
-        
-        List<Integer> intList = new ArrayList();
-        
-        Arrays.stream(String.valueOf(x).split("")).forEach(a -> intList.add(Integer.parseInt(a)));
-        
-        int sum = 0;
-        for(int i=0;i<intList.size();i++) {
-            sum+=intList.get(i);
-        }
-        
-        if(x%sum == 0) answer = true;
-        
-        return answer;
+        return x % String.valueOf(x).chars().map(ch -> ch - '0').sum() == 0;
     }
 }
