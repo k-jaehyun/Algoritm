0aacf163a6c753bae48f075ffca768fb241e66c8
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-12-11 12:46:06 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,17 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        String tr = "";
+        List<Integer> list = new ArrayList<>();
         
-        while(n>=3) {
-            tr+=n%3;
-            n/=3;
+        while(n>0) {
+            list.add(n%3);
+            n=n/3;
         }
-        tr+=n;
         
-        for(int i=0;i<tr.length();i++) {
-            answer+=(tr.charAt(tr.length()-i-1)-'0') * Math.pow(3,i);
+        int size = list.size();
+        
+        for(int i=0;i<size;i++) {
+            int temp = 1;
+            for(int j=i+1;j<size;j++) {
+                temp*=3;
+            }
+            answer+=temp*list.get(i);
         }
         
         return answer;
