544f0902357733e1653f7cdb281aee0ef00023ce
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-12-16 10:30:11 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,16 +1,18 @@
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
+        int len = number.length;
         
-        for(int i=0;i<number.length;i++) {
-            for(int j=i+1;j<number.length;j++) {
-                for(int k=j+1;k<number.length;k++) {
+        for(int i=0;i<len;i++) {
+            for(int j=i+1;j<len;j++) {
+                for(int k=j+1;k<len;k++) {
                     if(number[i]+number[j]+number[k] == 0) {
                         answer++;
                     }
                 }
             }
         }
+        
         
         return answer;
     }
