9410b88ebd42adc0aed81068b277477fa6dd1fa2
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-12-19 12:24:40 +0900

    [level 1] Title: K번째수, Time: 0.52 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        
         int[] answer = new int[commands.length];
-
-        for(int i=0;i<commands.length;i++) {
-            List<Integer> comArr = new ArrayList<Integer>();
-            for(int j=0;j<commands[i][1]-commands[i][0]+1;j++) {
-                comArr.add(array[commands[i][0]+j-1]);
-            }
-            Collections.sort(comArr);
-            answer[i] = comArr.get(commands[i][2]-1);
+        int index=0;
+        for(int[] command : commands) {
+            int[] arr = Arrays.copyOfRange(array, command[0]-1,command[1]);
+            Arrays.sort(arr);
+            answer[index] = arr[command[2]-1];
+            index++;
         }
         
         return answer;
