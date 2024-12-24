59b7c2f6b8e42233e15121c948dfdd7700645748
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-12-25 01:23:23 +0900

    [Silver III] Title: 크리스마스 선물, Time: 1024 ms, Memory: 172416 KB -BaekjoonHub

--- 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
@@ -5,22 +5,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        List<Integer> list = new ArrayList<>();
+        PriorityQueue<Integer> que = new PriorityQueue<>(Comparator.reverseOrder());
         
         for(int i=0;i<n;i++) {
             int a = sc.nextInt();
             if(a>0) {
                 for(int j=0;j<a;j++) {
                     int x = sc.nextInt();
-                    list.add(x);
+                    que.add(x);
                 }
-                Collections.sort(list, Comparator.reverseOrder());
             } else {
-                if(list.size() == 0) {
+                if(que.size() == 0) {
                     System.out.println(-1);
                 } else {
-                    System.out.println(list.get(0));
-                    list.remove(0);
+                    System.out.println(que.poll());
                 }
             }
         }
