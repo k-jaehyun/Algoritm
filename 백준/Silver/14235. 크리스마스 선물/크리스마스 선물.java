84bd8de43ef8b225dcd7ec6464081a1f246790b5
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-12-25 01:39:54 +0900

    [Silver III] Title: 크리스마스 선물, Time: 388 ms, Memory: 35544 KB -BaekjoonHub

--- 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
@@ -1,17 +1,21 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine());
         
         PriorityQueue<Integer> que = new PriorityQueue<>(Comparator.reverseOrder());
         
         for(int i=0;i<n;i++) {
-            int a = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            
             if(a>0) {
                 for(int j=0;j<a;j++) {
-                    int x = sc.nextInt();
+                    int x = Integer.parseInt(st.nextToken());
                     que.add(x);
                 }
             } else {
