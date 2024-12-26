ffbf7269a82f3dca274ec4ad2388ddb3deee56bd
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-12-26 11:18:00 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,16 +1,17 @@
 class Solution {
     public String solution(int a, int b) {
         String answer = "";
-        String[] week = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
-        int[] day = {31,29,31,30,31,30,31,31,30,31,30,31};
-        int num = 0;
         
+        String[] week = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] day = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int total=0;
         for(int i=0;i<a-1;i++) {
-            num += day[i];
+            total+=day[i];
         }
         
-        answer = week[(num+b-1)%7];
-        
+        answer = week[(total+b-1)%7];
+            
         return answer;
     }
 }
