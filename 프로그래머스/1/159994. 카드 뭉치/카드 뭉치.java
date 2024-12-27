384be8383feb99bd568e4a280506d8eecd8da782
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2024-12-27 10:28:48 +0900

    [level 1] Title: 카드 뭉치, Time: 0.10 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
         
-        List<String> cards1List = new ArrayList<>();
-        List<String> cards2List = new ArrayList<>();
+        List<String> cards1List = new ArrayList<>(Arrays.asList(cards1));
+        List<String> cards2List = new ArrayList<>(Arrays.asList(cards2));
         
-        for(int i=0;i<cards1.length;i++) {
-            cards1List.add(cards1[i]);
-        }
-        for(int i=0;i<cards2.length;i++) {
-            cards2List.add(cards2[i]);
-        }        
-        
-        for(int i=0;i<goal.length;i++) {
-            if(cards1List.indexOf(goal[i])==0) {
-                cards1List.remove(0);
-            } else if(cards2List.indexOf(goal[i])==0) {
-                cards2List.remove(0);
-            } else {
-                return "No";
+        for(String str:goal) {
+            if(cards1List.size()!=0) {
+                if(str.equals(cards1List.get(0))) {
+                    cards1List.remove(0);
+                    continue;
+                }
             }
+            if(cards2List.size()!=0) {
+                if(str.equals(cards2List.get(0))) {
+                    cards2List.remove(0);
+                    continue;
+                }
+            }
+            return "No";
         }
         
         return "Yes";
