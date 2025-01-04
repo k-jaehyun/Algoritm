db2641ed73fb4b708f19cf160b7940bb7093bcb6
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2025-01-05 03:09:34 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,68 +1,55 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = {0,0};
-        int cnt=0;
-        int x=0;
+        int maxCnt=0;
+        int minCnt=0;
         
+        // 최저
         for(int i=0;i<6;i++) {
-            if(lottos[i]!=0) {
-                loof1: for(int j=0;j<6;j++) {
-                    if(lottos[i]==win_nums[j]) {
-                        cnt++;
-                        break loof1;
-                    }
-                }
-            } else{
-            x++;
-                
+            for(int j=0;j<6;j++) {
+                if(lottos[i] == win_nums[j]) {
+                    minCnt++;
+                }    
             }
         }
+        switch(minCnt) {
+            case 6: answer[1]=1;
+                break;    
+            case 5: answer[1]=2;
+                break;
+            case 4: answer[1]=3;
+                break;
+            case 3: answer[1]=4;
+                break;
+            case 2: answer[1]=5;
+                break;
+            default: answer[1]=6;
+                break;
+        }
         
-        x+=cnt;
+        // 최대
+        for(int i=0;i<6;i++) {
+            if(lottos[i]==0) {
+                maxCnt++;
+            }
+        }
+        maxCnt+=minCnt;
+        switch(maxCnt) {
+            case 6: answer[0]=1;
+                break;    
+            case 5: answer[0]=2;
+                break;
+            case 4: answer[0]=3;
+                break;
+            case 3: answer[0]=4;
+                break;
+            case 2: answer[0]=5;
+                break;
+            default: answer[0]=6;
+                break;
+        }
         
-        switch(x) {
-            case 6: 
-                    answer[0]=1;
-                break;
-            case 5:
-                    answer[0]=2;
-                break;
-            case 4:
-                    answer[0]=3;
-                break;
-            case 3:
-                    answer[0]=4;
-                break;
-            case 2:
-                    answer[0]=5;
-                break;
-            case 1:
-            case 0:
-                    answer[0]=6;
-                break;
-        }        
         
-                switch(cnt) {
-                    case 6:
-                    answer[1]=1;
-                break;
-                    case 5:
-                    answer[1]=2;
-                break;
-                    case 4:
-                    answer[1]=3;
-                break;
-                    case 3:
-                    answer[1]=4;
-                break;
-                    case 2:
-                    answer[1]=5;
-                break;
-                    case 1:
-                    case 0:
-                    answer[1]=6;
-                break;
-        }     
         
         return answer;
     }
