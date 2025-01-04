f6310221c4194c2f6d88a26f7e8c40f0b7f60794
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2025-01-05 02:17:19 +0900

    [level 1] Title: 덧칠하기, Time: 2.42 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,29 +1,12 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
+        int answer = 1;
+        int roller = section[0];
         
-        // 0인 배열(벽) 만들고 칠해야하는 부분을 1로
-        int[] arr = new int[n];
         for(int i=0;i<section.length;i++) {
-            arr[section[i]-1] = 1;
-        }
-        
-        // 1인지 확인 후 m만큼 칠한다
-        for(int i=0;i<=n-m;i++) {
-            if(arr[i]==1) {
+            if(roller+m-1 < section[i]) {
                 answer++;
-                for(int j=i;j<i+m;j++) {
-                    arr[j]=0;
-                }
-                i+=m-1;
-            }
-        }
-        
-        // 끝부분 확인
-        for(int i=n-m;i<n;i++) {
-            if(arr[i]==1) {
-                answer++;
-                break;
+                roller = section[i];
             }
         }
         
