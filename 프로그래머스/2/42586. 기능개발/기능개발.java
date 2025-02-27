51cc9ebc0c3727e6e7e0d54f016829f39e79ee72
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2025-02-27 11:14:26 +0900

    [level 2] Title: 기능개발, Time: 2.66 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,36 +3,28 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
+        int index=0;
         List<Integer> list = new ArrayList<>();
-        int end = 0;
-        while(end<progresses.length) {
+        
+        while(index<progresses.length) {
             int tmp=0;
             for(int i=0;i<progresses.length;i++) {
                 progresses[i] += speeds[i];
             }
             for(int i=0;i<progresses.length;i++) {
-                if(progresses[i]>=100) {
-                    if(progresses[i]>=10000) {
-                        continue;
-                    }
-                    end++;
-                    tmp++;
-                    progresses[i]=10000;
-                    continue;
+                if(progresses[i]<100) {
+                    break;
                 }
-                break;
+                tmp++;
             }
             
-            if(tmp!=0) {
-                list.add(tmp);
+            if(tmp!=index) {
+                list.add(tmp-index);
             }
+            index=tmp;
         }
         
-        int[] answer = new int[list.size()];
-        for(int i=0;i<list.size();i++) {
-            answer[i]=list.get(i);
-        }
         
-        return answer;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
