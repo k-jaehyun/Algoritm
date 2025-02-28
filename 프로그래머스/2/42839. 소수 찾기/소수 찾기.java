0be69410c360adafcc7caca6304bbaddc7f409fa
k-jaehyun <70873780+k-jaehyun@users.noreply.github.com>
2025-02-28 23:22:24 +0900

    [level 2] Title: 소수 찾기, Time: 16.25 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,51 +1,40 @@
-import java.io.*;
 import java.util.*;
+
 class Solution {
-    static ArrayList<Integer> arr = new ArrayList<>();
-    static boolean[] check = new boolean[7];
+    
+    public Set<Integer> set = new HashSet<>();
+    public boolean[] visited;
+    public String[] numberArr;
+    public int answer = 0;
     
     public int solution(String numbers) {
-        int answer = 0;
-        for(int i=0; i<numbers.length(); i++){
-            dfs(numbers,"",i+1);
-        }
+        visited = new boolean[numbers.length()];
+        numberArr = numbers.split("");
+        dfs(0, "");
         
-        for(int i=0; i<arr.size(); i++){
-            if(prime(arr.get(i))) answer++;              
+        a: for(int n:set) {
+            if(n==0 || n==1) {
+                continue a;
+            }
+            for(int i=2;i*i<=n;i++) {
+                if(n%i==0) {
+                    continue a;
+                }
+            }
+            answer++;
         }
         
         return answer;
-  
     }
-	//백트래킹
-	static void dfs(String str, String temp, int m) {
-            if(temp.length() == m){
-                int num = Integer.parseInt(temp);
-                if(!arr.contains(num)){
-                    arr.add(num);
-                }
+    
+    public void dfs(int depth, String word) {
+        for(int i=0;i<numberArr.length;i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                set.add(Integer.valueOf(word+numberArr[i]));
+                dfs(depth+1, word+numberArr[i]);
+                visited[i] = false;
             }
-        
-            for(int i=0; i<str.length(); i++){
-                if(!check[i]){
-                    check[i] = true;
-                    temp += str.charAt(i);
-                    dfs(str, temp, m);
-                    check[i] = false;
-                    temp = temp.substring(0, temp.length()-1);
-                }
-            }
-		
-	}
-	//소수 판단
-	static boolean prime(int n) {
-		if(n<2) return false;
-		
-		for(int i=2; i*i<=n; i++) {
-			if(n % i == 0) return false;
-		}
-		
-		return true;
-	}
-
+        }
+    }
 }
